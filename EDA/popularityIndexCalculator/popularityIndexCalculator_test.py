6d3edaf7caf6da7c64ab64ef4d97c8d85adbc8c3
leehj24 <hyunji4579@gmail.com>
2025-05-18 00:14:14 +0900

    최종3

--- EDA/popularityIndexCalculator/popularityIndexCalculator_test.py
+++ EDA/popularityIndexCalculator/popularityIndexCalculator_test.py
@@ -1,6 +1,7 @@
 from popularityIndexCalculator import PopularityIndexCalculator
 
-calculator = PopularityIndexCalculator("./data/zb_review_data_final.csv")
+file = r'E:\machin-prj\EDA\popularityIndexCalculator\zb_review_data_final.csv'
+calculator = PopularityIndexCalculator(file)
 sorted_result_df = calculator.run_full_process()
 print(sorted_result_df.head())
-calculator.save_result("./data/popularity_index_result.csv")
+calculator.save_result("./EDA/popularityIndexCalculator/popularity_index_result.csv")
